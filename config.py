94973ddbd22a242b220320ab96af9ab19a9cdee6
David <dgkilolo23@gmail.com>
2020-05-15 09:39:20 +0300

    deployment.

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
 
     SECRET_KEY = '1234'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://dkg:boot@localhost/jipange'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://dkg:boot@localhost/jipange'
 
     @staticmethod
     def init_app(app):
